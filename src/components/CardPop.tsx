fe92141dbf00fd0129b03e83a23dfd86ebf63b5c
GeorgeUbong <botchedino@gmail.com>
2025-11-28 16:52:31 +0100

    newere

--- src/components/CardPop.tsx
+++ src/components/CardPop.tsx
@@ -12,8 +12,8 @@ export default function CardPop({ onClose, onCreate }: CardProps) {
 
 
     return (
-        <div>
-            <h3>Enter a task name</h3>
+        <div style={styles.popup}>
+            <h3 style={styles.head}>Enter a task name</h3>
             <input 
             type="text"
             placeholder="what are you thinking of"
@@ -21,13 +21,35 @@ export default function CardPop({ onClose, onCreate }: CardProps) {
             onChange={(e) => setTaskName(e.target.value)}
              />
 
-             <button className='button-nav' onClick={() => {
+             <div style={styles.buttonClass}>
+                <button className='button-nav' onClick={() => {
                 if (taskName.trim().length > 0) {
                     onCreate(taskName);
                     setTaskName("");
                 }
              }}>Create</button>
              <button className="close-butt" onClick={onClose}> Cancel</button>
+             </div>
         </div>
     )
+}
+
+
+const styles: { [key: string]: React.CSSProperties } = {
+popup:{
+    position: 'absolute',
+    border: '1px solid black',
+    padding: '30px',
+    zIndex: 90,
+    backgroundColor: 'white',
+    borderRadius: '10px',
+},
+head:{
+    fontWeight: '700',
+    fontSize: '20px',
+    padding: '5px',
+},
+buttonClass: {
+    padding: '30px',
+}
 }
